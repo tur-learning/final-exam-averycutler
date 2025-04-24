ff1ae30775e6e179b55a52aa5d139b933d3b1753
Avery Cutler <tut70526@temple.edu>
2025-04-24 08:37:35 +0000

    commit

--- final-project/mast3r.py
+++ final-project/mast3r.py
@@ -18,7 +18,7 @@ def master(input):
     print(filelist)
 
     # Step 3: Use gradio_client to call the endpoint
-    HF_TOKEN = "********"
+    HF_TOKEN = "******"
 
     if HF_TOKEN is None:
         raise ValueError("ERROR: YOU MUST INPUT YOUR HUGGINGFACE TOKEN")
